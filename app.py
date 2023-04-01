f6ec15a29e56f00b78adb762e604fe9123360568
Karol Żyłka <zylkakarol@gmail.com>
2023-04-01 17:51:55 +0200

    Update First_website

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route('/mypage/me', methods=['GET'])
+def me():
+    return render_template("me.html")
+  
+
+@app.route('/mypage/contact', methods=['GET', 'POST'])
+def contact():
+   if request.method == 'GET':
+       print("We received GET")
+       return render_template("contact.html")
+   elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return render_template("contact.html")
+
+
